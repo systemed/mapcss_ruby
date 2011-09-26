2dac68785e773a4f6cdcf7aa38a620073cf6d2b8
Richard Fairhurst <richard@systemeD.net>
2011-09-26 10:22:32 +0100

    More relation-handling methods

--- lib/style_parser/dictionary.rb
+++ lib/style_parser/dictionary.rb
@@ -48,9 +48,37 @@ module StyleParser
 				parent.type=='way'
 			end
 		end
+		
+		def parent_relations_of_type(child,type,role=nil)
+			parent_objects(child).collect do |parent|
+				if parent.type=='relation' && 
+				   parent.tags['type'] && parent.tags['type']==type &&
+				   (!role || parent.member(child.type,child.id).role==role) then parent end
+			end.compact
+		end
+		
+		def is_member_of(child,type,role=nil)
+			parent_objects(child).each do |parent|
+				if parent.type=='relation' &&
+				   parent.tags['type'] && parent.tags['type']==type &&
+				   (!role || parent.member(child.type,child.id).role==role) then return true end
+			end
+			false
+		end
+		
+		def relation_loaded_members(database,relation,role=nil)
+			relation.members.collect do |member|
+                obj = case member.type
+                    when 'node'     then database.get_node(member.ref)
+                    when 'way'      then database.get_way(member.ref)
+                    when 'relation' then database.get_relation(member.ref)
+                end
+                if !role || member.role==role then obj end
+            end.compact
+		end
 
 		private
-		
+
 		def add(parent,child)
 			if @dictionary[child] and !@dictionary[child].index(parent)
 				@dictionary[child].push(parent)
@@ -59,16 +87,5 @@ module StyleParser
 			end
 		end
 		
-		def relation_loaded_members(database,relation)
-            relation.members.collect do |member|
-                obj = case member.type
-                    when 'node'     then database.get_node(member.ref)
-                    when 'way'      then database.get_way(member.ref)
-                    when 'relation' then database.get_relation(member.ref)
-                end
-                obj
-            end.compact
-		end
-
 	end
 end
